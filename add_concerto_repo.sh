819745be8503ab2738a7ce268a9f71210680e83d
August <august@inniogroup.com>
2012-06-01 14:11:48 -0400

    Added concerto repo install script

---
+++ add_concerto_repo.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+wget -O - http://dl.concerto-signage.org/concerto_deb_public.key | sudo apt-key add -
+
+echo 'deb http://dl.concerto-signage.org/packages/ precise main' > /tmp/concerto.list
+sudo cp -fr /tmp/concerto.list /etc/apt/sources.list.d/
+rm /tmp/concerto.list
+sudo apt-get update
