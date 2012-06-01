ebcc5bf8e4739bb4dcd44724d8d6a6bd79d746df
August <august@inniogroup.com>
2012-06-01 13:57:11 -0400

    Added more build scripting, signed repository, and more post install tasks to test

--- build_deb_package.sh
+++ build_deb_package.sh
@@ -4,3 +4,10 @@ find ./debian -type d | xargs chmod 755
 fakeroot dpkg-deb --build debian
 mv debian.deb Concerto_0.1-1_all.deb
 lintian Concerto_0.1-1_all.deb
+
+mkdir -p packages/conf
+cp distributions packages/conf/
+cd packages
+reprepro --ask-passphrase -Vb . includedeb precise /home/august/concerto-debian/Concerto_0.1-1_all.deb
+cd ..
+tar -czvf packages.tar.gz packages
