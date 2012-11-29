26ad080229ec7d1f588de90c8e563488220e4b4d
August <august@inniogroup.com>
2012-11-29 15:30:56 -0500

    Added webserver restart to postinstall...package now installs without problems

--- build_deb_package.sh
+++ build_deb_package.sh
@@ -4,6 +4,15 @@
 ruby get_version_tag.rb
 version=`cat VERSION`
 
+rm -rf debian/usr/share/concerto
+git submodule init
+git submodule update
+cd debian/usr/share/concerto
+git checkout $version
+git submodule init
+git submodule update
+cd ../../../../
+
 sed -i -e "s/^.*Version.*$/Version: ${version}/" debian/DEBIAN/control
 
 find ./debian -type d | xargs chmod 755
@@ -15,6 +24,6 @@ rm -rf packages.tar.gz packages/
 mkdir -p packages/conf
 cp distributions packages/conf/
 cd packages
-reprepro --ask-passphrase -Vb . includedeb precise /home/august/concerto-debian/Concerto_${version}_all.deb
+reprepro --ask-passphrase -Vb . includedeb precise ~/concerto-debian/Concerto_${version}_all.deb
 cd ..
 tar -czvf packages.tar.gz packages
