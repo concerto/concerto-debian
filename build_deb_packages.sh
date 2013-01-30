0a5db3d5f2c650a54260d9effbcedfd18c546355
August <august@inniogroup.com>
2013-01-30 15:50:46 -0500

    Bifurcating the package per #5, but I've run into git problems with submodules - which may have broken the repository

---
+++ build_deb_packages.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+#Fetch Concerto version tag from Github and read the flatfile for the number
+ruby get_version_tag.rb
+version=`cat VERSION`
+
+rm -rf concerto_full/usr/share/concerto
+git submodule init
+git submodule update
+cd concerto_full/usr/share/concerto
+git checkout $version
+git submodule init
+git submodule update
+cd ../../../../
+sed -i -e "s/^.*Version.*$/Version: ${version}/" concerto_full/DEBIAN/control
+
+rm -rf concerto_lite/usr/share/concerto
+git submodule init
+git submodule update
+cd concerto_lite/usr/share/concerto
+git checkout $version
+git submodule init
+git submodule update
+cd ../../../../
+sed -i -e "s/^.*Version.*$/Version: ${version}/" concerto_lite/DEBIAN/control
+
+find ./concerto_full -type d | xargs chmod 755
+fakeroot dpkg-deb --build concerto_full
+mv debian.deb concerto_full_${version}_all.deb
+lintian concerto_full_${version}_all.deb
+
+find ./concerto_lite -type d | xargs chmod 755
+fakeroot dpkg-deb --build concerto_lite
+mv debian.deb concerto_lite_${version}_all.deb
+lintian concerto_lite__${version}_all.deb
+
+rm -rf packages.tar.gz packages/
+mkdir -p packages/conf
+cp distributions packages/conf/
+cd packages
+reprepro --ask-passphrase -Vb . includedeb quantal ~/concerto-debian/concerto_full_${version}_all.deb
+reprepro --ask-passphrase -Vb . includedeb quantal ~/concerto-debian/concerto_lite_${version}_all.deb
+cd ..
+tar -czvf packages.tar.gz packages
