abbe5ec692041186005b42394876f5ca2b09702a
Marvin Frederickson <marvin.frederickson@gmail.com>
2013-08-23 16:31:42 -0800

    quiet git fix more perms

--- build_deb_packages.sh
+++ build_deb_packages.sh
@@ -20,38 +20,60 @@ fi
 
 # update concerto_full's local repo to the version requested
 echo "  updating concerto_full's local repo..."
-cd concerto_full/usr/share/concerto
-git checkout .
-git pull origin $version
-git checkout $version
-cd ../../../../
-sed -i -e "s/^.*Version.*$/Version: ${version}/" concerto_full/DEBIAN/control
+cd concerto_full
 
-# set permissions, build package and check it
+cd usr/share/concerto
+git reset --hard -q
+git checkout master -q
+git pull origin master -q
+git checkout $version -q
+localversion="$(git describe --always --tags)"
+cd ../../../
+sed -i -e "s/^.*Version.*$/Version: ${version}/" DEBIAN/control
+echo "  local repo at ${localversion}"
+
+# set permissions
+echo "  setting permissions for control files..."
+cd DEBIAN
+chmod 644 *
+chmod 755 config postinst postrm preinst prerm
+cd ../etc/init.d
+chmod 755 concerto
+cd ../..
+chmod 644 etc/apache2/sites-available/concerto
+
 echo "  setting directories to 755..."
-find ./concerto_full -type d | xargs chmod 755
+find ./ -type d | xargs chmod 755
+chmod 755 usr/local/bin/concerto-install-mysql
 echo "  setting files to 644..."
-find ./concerto_full/usr/share/concerto -type f -perm 664 | xargs chmod 644
-find ./concerto_full/usr/share/concerto -regextype posix-awk -regex "(.*\.png|.*\.jpg|.*\.ttf|.*\.pdf|.*\.eot|.*\.svg)" | xargs chmod 644
-chmod 755 ./concerto_full/usr/share/concerto/concerto
+find ./usr/share/concerto -type f -perm 664 | xargs chmod 644
+find ./usr/share/concerto -regextype posix-awk -regex "(.*\.png|.*\.jpg|.*\.ttf|.*\.pdf|.*\.eot|.*\.svg)" | xargs chmod 644
+chmod 755 ./usr/share/concerto/concerto
 echo "  setting files to 755..."
-find ./concerto_full/usr/share/concerto -type f -perm 775 | xargs chmod 755
+find ./usr/share/concerto -type f -perm 775 | xargs chmod 755
+cd ..
+
+# build package 
 echo "  building package..."
 fakeroot dpkg-deb --build concerto_full
 echo "  renaming package..."
 mv concerto_full.deb concerto_full_${version}_all.deb
+
 echo "  checking package..."
 lintian -i concerto_full_${version}_all.deb > full_lintian.log
-echo "  $(grep "E: " full_lintian.log | wc -l) errors, $(grep "W: " full_lintian.log | wc -l) warnings"
+echo "  $(grep "E: " full_lintian.log | wc -l) errors"
 grep "E: " full_lintian.log | sed 's/^/    /'
+echo "  $(grep "W: " full_lintian.log | wc -l) warnings"
+grep "W: " full_lintian.log | sed 's/^/    /'
+
 exit 0
 
 
 # update concerto_lite's local repo to the version requested
 echo "  updating concerto_lite's local repo..."
 cd concerto_lite/usr/share/concerto
-git checkout .
-git pull origin $version
+git checkout master
+git pull origin master
 git checkout $version
 cd ../../../../
 sed -i -e "s/^.*Version.*$/Version: ${version}/" concerto_lite/DEBIAN/control
