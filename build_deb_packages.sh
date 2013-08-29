7b31d62d2a2da242a2fbc860dff905bb69f89a9f
Marvin Frederickson <marvin.frederickson@gmail.com>
2013-08-29 11:39:41 -0800

    fix perm problem, make quieter, handle master

--- build_deb_packages.sh
+++ build_deb_packages.sh
@@ -17,7 +17,7 @@ function update_local_repo() {
   git checkout $version -q
   localversion="$(git describe --always --tags)"
   cd ../../..
-  sed -i -e "s/^.*Version.*$/Version: ${version}/" DEBIAN/control
+  sed -i -e "s/^.*Version.*$/Version: ${control_version}/" DEBIAN/control
   echo "  local repo at ${localversion}"
   cd ..
 }
@@ -33,7 +33,7 @@ function build_package() {
   echo "  building package..."
   fakeroot dpkg-deb --build concerto_${1} debs
   echo "  checking package... (results logged to: ${1}_lintian.log)"
-  lintian -i --show-overrides debs/concerto-${1}_${version}_all.deb > ${1}_lintian.log
+  lintian -i --show-overrides debs/concerto-${1}_${control_version}_all.deb > ${1}_lintian.log
   echo "    $(grep "E: " ${1}_lintian.log | wc -l) errors"
   grep "E: " ${1}_lintian.log | sed 's/^/      /'
   echo "    $(grep "W: " ${1}_lintian.log | wc -l) warnings"
@@ -57,15 +57,14 @@ function set_permissions() {
     chmod 644 etc/apache2/sites-available/concerto
   fi
   chmod 644 ./usr/share/lintian/overrides/concerto-${1}
-  echo "  setting directories to 755..."
+  #echo "  setting directories to 755..."
   find ./ -type d | xargs chmod 755
-  echo "  setting files to 644..."
+  #echo "  setting files to 644..."
   find ./usr/share/concerto -type f -perm 664 | xargs chmod 644
   find ./usr/share/concerto -regextype posix-awk -regex "(.*\.png|.*\.jpg|.*\.ttf|.*\.pdf|.*\.eot|.*\.svg|.*\.woff)" | xargs chmod 644
   chmod 644 ./usr/share/doc/concerto-${1}/*
-  chmod 755 ./usr/share/concerto/concerto
-  echo "  setting files to 755..."
-  find ./usr/share/concerto -type f -perm 775 | xargs chmod 755
+  #echo "  setting files to 755..."
+  find ./usr/share/concerto -type f -perm 775 -exec chmod 755 '{}' \;
   cd ..
 }
 
@@ -86,18 +85,57 @@ function copy_service_script() {
     exit 1
   fi
 
-  cd concerto_${1}
-  echo "  copying service script from repo..."
-  cp usr/share/concerto/concerto-init.d etc/init.d/concerto
-  cd ..
+  echo "  copying service script from repo... "
+  mkdir -p concerto_${1}/etc/init.d
+  cp concerto_${1}/usr/share/concerto/concerto-init.d concerto_${1}/etc/init.d/concerto
+  # remove it from the repo directory so the packager doesnt complain
+  rm concerto_${1}/usr/share/concerto/concerto-init.d
 }
+
+# ---------------------------------------------------
+# main
+# ---------------------------------------------------
+# get options
+# run with -v master      to generate a package based on the master branch
+#
+# Although you could also use this to build a prior tagged version, that's not 
+# recommended because there may have been changes to this build script since then.
+
+override_version=""
+while getopts ":v:" opt; do
+  case $opt in
+    v)
+      override_version="${OPTARG}"
+      ;;
+    \?)
+      echo "invalid option: -${OPTARG}" >&2
+      exit 1;
+      ;;
+    :)
+      echo "option -${OPTARG} requires an argument" >&2
+      exit 1;
+      ;;
+  esac
+done
 
 # ---------------------------------------------------
 # Fetch Concerto version tag from Github and read the flatfile for the number
 # ---------------------------------------------------
 ruby get_version_tag.rb
 version=`cat VERSION`
-echo -e "\nBuilding packages for VERSION ${version}\n"
+if [ "${override_version}" != "" ]; then
+  echo -e "\nBuilding packages for VERSION ${override_version} but repo is at ${version} !!\n"
+  version="${override_version}"
+else
+  echo -e "\nBuilding packages for VERSION ${version}\n"
+fi
+
+# update the control_version variable which is used for buidling the package
+if [ "${version}" == "master" ]; then
+  control_version="0.0.0.${version}"
+else
+  control_version="${version}"
+fi
 
 # ---------------------------------------------------
 # init and update the submodules if needed
@@ -142,9 +180,9 @@ mkdir -p packages/conf
 cp distributions packages/conf/
 cd packages
 echo "  preparing concerto_full package..."
-reprepro --component main --ask-passphrase -vb . includedeb raring ../debs/concerto-full_${version}_all.deb
+reprepro --component main --ask-passphrase -vb . includedeb raring ../debs/concerto-full_${control_version}_all.deb
 echo "  preparing concerto_lite package..."
-reprepro --component main --ask-passphrase -vb . includedeb raring ../debs/concerto-lite_${version}_all.deb
+reprepro --component main --ask-passphrase -vb . includedeb raring ../debs/concerto-lite_${control_version}_all.deb
 cd ..
 tar -czf packages.tar.gz packages
 
