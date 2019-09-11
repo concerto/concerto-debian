59f8aa9578148f07dce65750a21f576c47ec24ca
Marvin Frederickson <marvin.frederickson@gmail.com>
2019-09-11 09:09:04 -0800

    fix codename

--- scripts/add_repo.sh
+++ scripts/add_repo.sh
@@ -3,7 +3,17 @@
 sudo apt-get install apt-transport-https
 wget -O - http://dl.concerto-signage.org/concerto_deb_public.key | sudo apt-key add -
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 561F9B9CAC40B2F7
-echo 'deb http://dl.concerto-signage.org/packages/ $(lsb_release -cs) main\ndeb https://oss-binaries.phusionpassenger.com/apt/passenger $(lsb_release -cs) main' > /tmp/concerto.list
+
+# try to determine codename since not all distros (especially docker images) have lsb_release installed
+CODENAME=$(lsb_release -cs)
+if [ "$CODENAME" = "" ]; then
+  CODENAME=$(cat /etc/os-release | grep VERSION_CODENAME | cut -d '=' -f 2)
+  if [ "$CODENAME" = "" ]; then
+    # fallback to stretch since xenial, bionic, and buster have os-release with VERSION_CODENAME
+    CODENAME="stretch"
+  fi
+fi
+echo "deb http://dl.concerto-signage.org/packages/ ${CODENAME} main\ndeb https://oss-binaries.phusionpassenger.com/apt/passenger ${CODENAME} main" > /tmp/concerto.list
 sudo cp -fr /tmp/concerto.list /etc/apt/sources.list.d/
 rm /tmp/concerto.list
 sudo apt-get update
